9d9c39d67910908e308011b907aeb4345509a2a8
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2023-06-05 19:50:56 +0700

    fix header navbar

--- src/Component/Header/header.js
+++ src/Component/Header/header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./header.css";
-import { Col, Layout, Menu, Row } from "antd";
+import { Col, Menu, Row } from "antd";
 import MobileNavbar from "./mobileNavbar";
 import logo from "../../assets/Images/common/header-logo.png";
 import mail_icon from "../../assets/Images/common/header-mail.png";
@@ -45,45 +45,37 @@ function HeaderComponent() {
   });
 
   return (
-    <>
-      <Layout>
-        <Row className="header__row">
-          <Col
-            className="header__col_logo"
-            sm={{ span: 12 }}
-            md={{ span: 8 }}
-            xl={{ span: 10 }}
-          >
-            <img className="image__logo" src={logo} alt="功美技建" />
-          </Col>
+    <Row className="header__row">
+      <Col
+        className="header__col_logo"
+        sm={{ span: 12 }}
+        md={{ span: 8 }}
+        xl={{ span: 10 }}
+      >
+        <img className="image__logo" src={logo} alt="功美技建" />
+      </Col>
 
-          {isMobile ? (
-            <MobileNavbar />
-          ) : (
-            <Col sm={{ span: 12 }} md={{ span: 16 }} xl={{ span: 14 }}>
-              <Row className="header__row_navbar">
-                <Col className="header__col_navbar" md={{ span: 18 }}>
-                  <Menu
-                    className="header__menu_navbar"
-                    // onClick={onClick}
-                    mode="horizontal"
-                    items={items}
-                  />
-                </Col>
-                <Col className="header__col_mail" md={{ span: 6 }}>
-                  <img
-                    src={mail_icon}
-                    className="header__icon"
-                    alt="mail icon"
-                  />
-                  <span className="header__text">お問い合わせはこちら</span>
-                </Col>
-              </Row>
+      {isMobile ? (
+        <MobileNavbar />
+      ) : (
+        <Col sm={{ span: 12 }} md={{ span: 16 }} xl={{ span: 14 }}>
+          <Row className="header__row_navbar">
+            <Col className="header__col_navbar" md={{ span: 18 }}>
+              <Menu
+                className="header__menu_navbar"
+                // onClick={onClick}
+                mode="horizontal"
+                items={items}
+              />
             </Col>
-          )}
-        </Row>
-      </Layout>
-    </>
+            <Col className="header__col_mail" md={{ span: 6 }}>
+              <img src={mail_icon} className="header__icon" alt="mail icon" />
+              <span className="header__text">お問い合わせはこちら</span>
+            </Col>
+          </Row>
+        </Col>
+      )}
+    </Row>
   );
 }
 
