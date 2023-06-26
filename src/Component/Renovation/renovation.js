5da210eae5b2e2bccd230a78328ebc198749d959
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2023-06-26 10:11:25 +0700

    Fix error simulation page

--- src/Component/Renovation/renovation.js
+++ src/Component/Renovation/renovation.js
@@ -12,14 +12,12 @@ const infoHeader = {
 
 function Renovation() {
   return (
-    <div className="renovation__container">
+    <>
       <ImageCover infoHeader={infoHeader} />
-      <Row className="renovation__row">
-        <Col className="renovation__col">
-          <p className="renovation__text">現在、施工事例のデータがありません</p>
-        </Col>
-      </Row>
-    </div>
+      <section className="renovation">
+        <p>現在、施工事例のデータがありません</p>
+      </section>
+    </>
   );
 }
 
