c2ea88a16afae0c7a3c9983ddb985087ae7b54f6
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2023-06-27 16:04:25 +0700

    fix drawer - mobile navbar

--- src/Component/Header/mobileNavbar.js
+++ src/Component/Header/mobileNavbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-// import "./header.css";
 import "./mobileNavbar.css";
-import { Button, Drawer, Menu, Row } from "antd";
+import { Button, Drawer, Menu } from "antd";
 import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
 import mail_icon from "../../assets/Images/common/header-mail.png";
 import phone_icon from "../../assets/Images/common/header-phone.png";
@@ -44,6 +43,7 @@ function MobileNavbar() {
     setCurrent(e.key);
     navigate(`/${e.key}`, { replace: true });
     setOpenMenu(false);
+    window.scrollTo(0, 0);
   };
 
   const showDrawer = () => {
@@ -58,9 +58,7 @@ function MobileNavbar() {
       <Button className="header__button_menu" onClick={showDrawer}>
         <MenuOutlined className="header__icon_menu" />
       </Button>
-
       <Drawer
-        style={{ position: "fixed" }}
         width="100vw"
         height="100%"
         className="header__drawer"
@@ -69,32 +67,30 @@ function MobileNavbar() {
         open={openMenu}
         closeIcon={<CloseOutlined className="header__icon_close" />}
       >
-        <Row className="header__row_menu">
-          <Menu
-            className="header__drawer_menu"
-            items={items}
-            selectedKeys={[current]}
-            onClick={onClick}
-          />
-          <Row className="header__row_contact" style={{ width: "100%" }}>
-            <Button className="header__row_button">
-              <img
-                src={phone_icon}
-                className="header__icon_mobile"
-                alt="phone icon"
-              />
-              <span className="header__contact_text">06-6844-7078</span>
-            </Button>
-            <Button className="header__row_button">
-              <img
-                src={mail_icon}
-                className="header__icon_mobile"
-                alt="mail icon"
-              />
-              <span className="header__contact_text">お問い合わせはこちら</span>
-            </Button>
-          </Row>
-        </Row>
+        <Menu
+          className="header__drawer_menu"
+          items={items}
+          selectedKeys={[current]}
+          onClick={onClick}
+        />
+        <div className="header__row_contact">
+          <a href="tel:0668447078" className="header__row_button">
+            <img
+              src={phone_icon}
+              className="header__icon_mobile"
+              alt="phone icon"
+            />
+            <span className="header__contact_text phone">06-6844-7078</span>
+          </a>
+          <a href="/contact-us" className="header__row_button">
+            <img
+              src={mail_icon}
+              className="header__icon_mobile"
+              alt="mail icon"
+            />
+            <span className="header__contact_text">お問い合わせはこちら</span>
+          </a>
+        </div>
       </Drawer>
     </>
   );
